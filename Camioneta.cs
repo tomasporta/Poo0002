b24b04d0014e279a1c642e3419eec4a7c3f59e1d
portatomas <lusianoporta@gmail.com>
2025-06-22 23:16:16 -0300

    Add files via upload

---
+++ Camioneta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico.Entidades
+{
+    public class Camioneta : Vehiculo
+    {
+        public int CapacidadCarga { get; set; }
+        public override decimal CalcularCostoUso() => 15 * (DateTime.Now.Year - Anio) + 0.5m * CapacidadCarga;
+        public override string Informar()
+        {
+            return base.Informar() + $" | Carga: {CapacidadCarga}kg | Costo Uso: ${CalcularCostoUso():0.00}";
+        }
+
+    }
+}
+
---
+++ Moto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico.Entidades
+{
+    public class Moto : Vehiculo
+    {
+        public int Cilindrada { get; set; }
+        public override decimal CalcularCostoUso() => 5 * (DateTime.Now.Year - Anio) + 0.1m * Cilindrada;
+        public override string Informar()
+        {
+            return base.Informar() + $" | Cilindrada: {Cilindrada}cc | Costo Uso: ${CalcularCostoUso():0.00}";
+        }
+
+    }
+}
